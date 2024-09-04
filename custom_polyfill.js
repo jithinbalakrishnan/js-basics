b3f227ec223b57cd40566a22b5ec16c1974b96fb
jithinbalu <jithinbalakrishnan00@gmail.com>
2024-09-04 09:53:39 +0530

    reduce polyfill

--- custom_polyfill.js
+++ custom_polyfill.js
@@ -1,6 +1,8 @@
 // polyfill basically is writing our own implementation of in-build function
 //ref: https://www.youtube.com/watch?v=Th3rZjfKKhI
 
+
+// --------------- map ------------------------//
 Array.prototype.customMap = function (callBack) {
     let item = [];
     // here this is array
@@ -17,6 +19,7 @@ Array.prototype.customMap = function (callBack) {
   
   console.log("result", result);
   
+  // --------------- filter ------------------------//
   Array.prototype.customFilter = function (callBack) {
     let temp = [];
     for (let i = 0; i < this.length; i++) {
@@ -32,3 +35,18 @@ Array.prototype.customMap = function (callBack) {
   let filterResult = data.customFilter((i) => i > 25);
   
   console.log("filterResult", filterResult);
+
+  // --------------- reduce ------------------------//
+  Array.prototype.customReduce = function(callBack, inititalValue) {
+    var accumulator = inititalValue;
+    for(let i=0; i<this.length; i++) {
+        accumulator = accumulator ? callBack(accumulator, this[i],i) : this[i]
+    }
+    return accumulator
+}
+console.log([1,2,3].reduce((a,c)=> {
+    return a = a+c
+}))
+console.log([1,2,3].customReduce((a,c)=> {
+    return a = a+c
+}))
